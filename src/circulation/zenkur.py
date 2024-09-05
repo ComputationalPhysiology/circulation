4b61412ce166128d01a6f936b28e56c288f6d767
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-09-05 09:11:06 +0200

    Add example for zenkur model

--- src/circulation/zenkur.py
+++ src/circulation/zenkur.py
@@ -18,13 +18,6 @@ class Zenkur(base.CirculationModel):
 
     def __init__(self, parameters: dict[str, float] | None = None, add_units=False):
         super().__init__(parameters, add_units=add_units)
-
-        ############ Chambers
-        # chambers = self.parameters["chambers"]
-
-        # self.E_LV = self.time_varying_elastance(**chambers["LV"])
-        # self.p_LV_func = lambda V, t: self.E_LV(t) * (V - chambers["LV"]["V0"])
-        self.var = {}
         self._initialize()
 
     @staticmethod
@@ -50,17 +43,29 @@ class Zenkur(base.CirculationModel):
             "C_PRSW_min": 25.9 * mmHg,
             "C_PRSW_max": 103.8 * mmHg,
             "BPM": 75.0 * units.ureg("1/minutes"),
+            "start_withdrawal": 200.0 * s,
+            "end_withdrawal": 400.0 * s,
+            "start_infusion": 500.0 * s,
+            "end_infusion": 700.0 * s,
+            "flow_withdrawal": -1.0 * mL / s,
+            "flow_infusion": 1.0 * mL / s,
         }
 
     @staticmethod
     def default_initial_conditions() -> dict[str, float]:
         V0lv = 7.144 * mL
+        totalVolume = 4825 * mL
+
+        meanVvU = (2700 + 3100) / 2
+        Va = totalVolume / (700 + meanVvU) * 700
+        Vv = totalVolume / (700 + meanVvU) * meanVvU
+
         return {
             "V_ES": 2 * V0lv,
             "V_ED": 3 * V0lv,
             "S": 0.5,
-            "Va": 700 * mL,
-            "Vv": 3_000 * mL,
+            "Va": Va,
+            "Vv": Vv,
         }
 
     def fHR(self, S):
@@ -84,38 +89,72 @@ class Zenkur(base.CirculationModel):
         return Vv0_min + (Vv0_max - Vv0_min) * (1 - S)
 
     def update_static_variables(self, t):
-        ...
-        # self.var["p_LV"] = self.p_LV_func(self.state["V_LV"], t)
+        V_ES = self.state["V_ES"]
+        V_ED = self.state["V_ED"]
+        S = self.state["S"]
+        Va = self.state["Va"]
+        Vv = self.state["Vv"]
+
+        Ca = self.parameters["Ca"]
+        Cv = self.parameters["Cv"]
+        Va0 = self.parameters["Va0"]
+        Vv0 = self.Vv0(S)
+        fHR = self.fHR(S)
+        R_TPR = self.R_TPR(S)
+        C_PRSW = self.C_PRSW(S)
+        Pa = (Va - Va0) / Ca  # Eq 17
+        Pcvp = (Vv - Vv0) / Cv  # Eq 17
+        IC = (Pa - Pcvp) / R_TPR  # Eq 18
+        ICO = fHR * (V_ED - V_ES)  # Eq 19
+
+        self.var["fHR"] = fHR
+        self.var["R_TPR"] = R_TPR
+        self.var["C_PRSW"] = C_PRSW
+        self.var["Vv0"] = Vv0
+
+        self.var["Pa"] = Pa  # Eq 17
+        self.var["Pcvp"] = Pcvp  # Eq 17
+        self.var["IC"] = IC  # Eq 18
+        self.var["ICO"] = ICO  # Eq 19
+        self.var["p_LV"] = self.p_LV_func(Vv, t)
+        # breakpoint()
+
+        if t > self.parameters["start_withdrawal"] and t < self.parameters["end_withdrawal"]:
+            self.var["I_ext"] = self.parameters["flow_withdrawal"]
+        elif t > self.parameters["start_infusion"] and t < self.parameters["end_infusion"]:
+            self.var["I_ext"] = self.parameters["flow_infusion"]
+        else:
+            self.var["I_ext"] = 0.0
 
     def p_LV_func(self, V_LV, t=0.0):
         P0_LV = self.parameters["P0_LV"]
         kE_LV = self.parameters["kE_LV"]
         V_ED0 = self.parameters["V_ED0"]
 
-        # print(f"V_LV = {V_LV}", "V_ED0 = ", V_ED0)
         # Eq 7
         return P0_LV * (np.exp(kE_LV * (V_LV - V_ED0)) - 1)
 
-    def V_ES(self, V_ED, C_PRSW, Pa):
-        P_ED = self.p_LV_func(V_ED)
+    def V_ES(self, V_ED, C_PRSW, Pa, t):
+        P_ED = self.p_LV_func(V_ED, t)
         V_ED0 = self.parameters["V_ED0"]
 
+        if Pa > P_ED:
+            return V_ED0
         # Eq 5
         return V_ED - ((C_PRSW * (V_ED - V_ED0)) / (Pa - P_ED))
 
-    def V_ED(self, V_ES, fHR, Pcvp):
+    def V_ED(self, V_ES, fHR, Pcvp, t):
         # Eq 13
-        p_LV_ES = self.p_LV_func(V_ES)
+        p_LV_ES = self.p_LV_func(V_ES, t)
         T_sys = self.parameters["T_sys"]
 
         if Pcvp > p_LV_ES:
             # Eq 12
-            return self.V((1 / fHR) - T_sys, Pcvp)
+            return self.V((1 / fHR) - T_sys, Pcvp, V_ES=V_ES)
         else:
             return V_ES
 
-    def V(self, t, Pcvp):
-        V_ES = self.state["V_ES"]
+    def V(self, t, Pcvp, V_ES):
         R_Valve = self.parameters["R_valve"]
         P0_LV = self.parameters["P0_LV"]
         kE_LV = self.parameters["kE_LV"]
@@ -124,7 +163,10 @@ class Zenkur(base.CirculationModel):
         # Eq 9
         k1 = -(P0_LV / R_Valve) * np.exp(-kE_LV * V_ED0)
         k2 = kE_LV
-        k3 = -(Pcvp + P0_LV) / R_Valve
+        # REMOVE MINUS SIGN
+        k3 = (Pcvp + P0_LV) / R_Valve
+
+        # breakpoint()
 
         return -(1 / k2) * np.log(
             (k1 / k3) * (np.exp(-k2 * k3 * t) - 1) + np.exp(-k2 * (V_ES + k3 * t))
@@ -133,44 +175,28 @@ class Zenkur(base.CirculationModel):
     def step(self, t, dt):
         self.update_static_variables(t)
 
-        Ca = self.parameters["Ca"]
-        Cv = self.parameters["Cv"]
-        Va0 = self.parameters["Va0"]
+        V_ES = self.state["V_ES"]
+        V_ED = self.state["V_ED"]
+        S = self.state["S"]
         tau_Baro = self.parameters["tau_Baro"]
         k_width = self.parameters["k_width"]
         Pa_set = self.parameters["Pa_set"]
 
-        I_ext = 0.0
+        fHR = self.var["fHR"]
+        C_PRSW = self.var["C_PRSW"]
+        Pa = self.var["Pa"]
+        Pcvp = self.var["Pcvp"]
+        IC = self.var["IC"]
+        ICO = self.var["ICO"]
+        I_ext = self.var["I_ext"]
 
-        V_ES = self.state["V_ES"]
-        V_ED = self.state["V_ED"]
-        S = self.state["S"]
-        Va = self.state["Va"]
-        Vv = self.state["Vv"]
+        # Added minus sign to match the other code
+        dVa_dt = -(IC - ICO)  # Eq 20
 
-        fHR = self.fHR(S)
-        R_TPR = self.R_TPR(S)
-        C_PRSW = self.C_PRSW(S)
-        Vv0 = self.Vv0(S)
-
-        Pa = (Va - Va0) / Ca  # Eq 17
-        Pcvp = (Vv - Vv0) / Cv  # Eq 17
-
-        IC = (Pa - Pcvp) / R_TPR  # Eq 18
-        ICO = fHR * (V_ED - V_ES)  # Eq 19
-
-        dVa_dt = IC - ICO  # Eq 20
+        dS_dt = (1 / tau_Baro) * (1 - (1 / (1 + np.exp(-k_width * (Pa - Pa_set)))) - S)
 
         self.state["Va"] += dt * dVa_dt
         self.state["Vv"] += dt * (-dVa_dt + I_ext)  # Eq 20
-        self.state["V_ES"] += dt * (self.V_ES(V_ED, C_PRSW, Pa) - V_ES) * fHR
-        self.state["V_ED"] += dt * (self.V_ED(V_ES, Vv, Pcvp) - V_ED) * fHR
-        self.state["S"] += (
-            dt * (1 / tau_Baro) * (1 - (1 / (1 + np.exp(-k_width * (Pa - Pa_set)))) - S)
-        )
-
-        self.results["time"].append(t)
-        self.results["V_LV"].append(self.state["Vv"])
-        self.results["p_LV"].append(self.p_LV_func(Vv, t))
-
-        # print(self.state)
+        self.state["V_ES"] += dt * (self.V_ES(V_ED, C_PRSW, Pa, t) - V_ES) * fHR
+        self.state["V_ED"] += dt * (self.V_ED(V_ES, fHR, Pcvp, t) - V_ED) * fHR
+        self.state["S"] += dt * dS_dt
